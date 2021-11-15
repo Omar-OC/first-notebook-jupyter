1eb6d9e143c211f4ffdbf7effd36aad8526000c6
Omar O <83517572+Omar-OC@users.noreply.github.com>
2021-11-15 02:59:52 -0500

    Served as a .py file

---
+++ 01_first_notebook.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Introduction
+# This is my *first* Jupyter notebook.
+
+# In[7]:
+
+
+x = 5
+y = 6
+z = x+y
+
+
+# In[8]:
+
+
+print(z)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
